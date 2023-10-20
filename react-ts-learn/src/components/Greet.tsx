51e1898c10dcd81517569e571d93d9d4fd519709
Jens Lindner <jelindner@t-online.de>
2023-10-20 14:09:02 +0200

    Tut State 5
    Tut Progress #5
    Step by step getting better!

--- react-ts-learn/src/components/Greet.tsx
+++ react-ts-learn/src/components/Greet.tsx
@@ -1,11 +1,19 @@
 type GreetProps = {
     name : string
+    messageCount? : number
+    isLoggedIn : boolean
 }
 
 const Greet = (props : GreetProps) => {
+
+    const {messageCount = 0} = props;
     return ( 
         <div>
-            <h2>Welcome {props.name}. You have 10 unread messages.</h2>
+            <h2>
+                {
+                    props.isLoggedIn ? `Welcome {props.name}. You have ${messageCount} unread messages.` : `welcome Guest`
+                }
+            </h2>
         </div>
      );
 }
