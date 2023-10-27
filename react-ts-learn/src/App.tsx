45c8d395a9444b6351ac182c6c6e34b19d4eecbc
Jens Lindner <jelindner@t-online.de>
2023-10-27 14:43:39 +0200

    Learn #5
    Chapter 5 learnings

--- react-ts-learn/src/App.tsx
+++ react-ts-learn/src/App.tsx
@@ -7,6 +7,8 @@ import PersonList from './components/PersonList';
 import Status from './components/Status';
 import Heading from './components/Heading';
 import Oscar from './components/Oscar';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
 
@@ -37,6 +39,10 @@ function App() {
         <Heading>Oscar goes to Leonardo Dicpario</Heading>
       </Oscar>
       <Greet name='Victor' isLoggedIn={false}/>
+      <Button handleClick={(event, id) => {
+        console.log("Button clicked", event, id);
+      }}/>
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
