e63449c062288cf2196f4ecdecaa9936698ab8db
Jens Lindner <jelindner@t-online.de>
2023-11-17 14:58:59 +0100

    #19 state
    #19 tut learning in progress

--- react-ts-learn/src/App.tsx
+++ react-ts-learn/src/App.tsx
@@ -14,6 +14,13 @@ import { Container } from './components/Container';
 import { ThemeContextProvider } from './components/context/ThemeContext';
 import { Box } from './components/context/Box';
 
+import { UserContextProvider } from './components/context/UserContext';
+import { User } from './components/state/User';
+import { Counter } from './components/class/Counter';
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
+
 function App() {
 
   const personName = {
@@ -51,6 +58,26 @@ function App() {
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
+      <UserContextProvider>
+          <User />
+      </UserContextProvider>
+      <Counter message='The count value is'/>
+      <Private isLoggedIn={true} component={Profile}/>
+      {/* <List items={['Batman','Superman','Wonder Woman']} onClick={(item) => console.log(item)} />
+      <List items={[1,2,3]} onClick={(item) => console.log(item)} /> */ }
+      <List items={[{
+        id: 1,
+        first: 'Bruce',
+        last: 'Wayne'
+      },{
+        id: 2,
+        first: 'Clark',
+        last: 'Kent'
+      },{
+        id: 3,
+        first: 'Princess',
+        last: 'Diana'
+      }]} onClick={(item) => console.log(item)} />
     </div>
   );
 }
---
+++ react-ts-learn/src/components/auth/Private.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import { Login } from "./Login";
+import { ProfileProps } from "./Profile";
+
+type PrivateProps = {
+
+    isLoggedIn: boolean
+    component: React.ComponentType<ProfileProps>
+}
+
+
+export const Private = ({isLoggedIn, component: Component} : PrivateProps) => {
+    if (isLoggedIn) {
+        
+        return <Component name="Yoshi" />
+    }else {
+        return <Login /> 
+    }
+}
